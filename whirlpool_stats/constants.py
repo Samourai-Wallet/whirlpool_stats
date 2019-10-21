af9abe87c9382b1a383b8aebd12abce7b6fac310
LaurentMT <laurentmt145@gmail.com>
2019-10-21 19:26:51 +0200

    Added file storing constants

---
+++ whirlpool_stats/constants.py
@@ -0,0 +1,19 @@
+'''
+Copyright (c) 2019 Katana Cryptographic Ltd. All Rights Reserved.
+
+Set of constants used by the scripts
+'''
+
+# OXT base url for whirpool snapshots
+BASE_URL_SNAPSHOTS = 'https://oxt.me/static/share/whirlpool'
+
+# Filename templates composing the snapshot for a given denomination
+FN_MIX_TXS = 'whirlpool_mix_txs'
+FN_TX0S = 'whirlpool_tx0s'
+FN_LINKS = 'whirlpool_links'
+
+FILENAME_TEMPLATES = [
+  FN_MIX_TXS,
+  FN_TX0S,
+  FN_LINKS
+]
